b703f97fc7499c96c3dfe775164e44e3c1234f87
yyamada12 <12yacropolisy@gmail.com>
2019-05-15 08:42:19 +0900

    [add] Interface bundle

--- MyOmikujiApp/ViewController.swift
+++ MyOmikujiApp/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    @IBOutlet weak var MyLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    @IBAction func getOmikuji(_ sender: Any) {
     }
 
 
