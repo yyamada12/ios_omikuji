8c97e99c31e5a43a64a7ee31affd4d3623c6553c
yyamada12 <12yacropolisy@gmail.com>
2019-05-14 22:16:04 +0900

    [add] label and button

---
+++ MyOmikujiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOmikujiApp
+//
+//  Created by Yoshinobu Yamada on 2019/05/14.
+//  Copyright © 2019 Yoshinobu Yamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
